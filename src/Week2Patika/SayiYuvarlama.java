2a09024c2ea8c4da45c32408c3e17813fad7b6bb
Burçin Üstün <burcin.ustun06@gmail.com>
2025-08-16 21:28:13 +0300

    week2

--- src/Week2Patika/SayiYuvarlama.java
+++ src/Week2Patika/SayiYuvarlama.java
@@ -1,4 +1,23 @@
 package Week2Patika;
 
-public class SayiYuvarlama {
+    import java.util.Scanner;
+
+    public class SayiYuvarlama {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Ondalıklı bir sayı giriniz: ");
+            double number = sc.nextDouble();
+
+            // Aşağı yuvarlama
+            System.out.println("Aşağı yuvarlama (floor): " + Math.floor(number));
+
+            // Yukarı yuvarlama
+            System.out.println("Yukarı yuvarlama (ceil): " + Math.ceil(number));
+
+            // En yakın tam sayıya yuvarlama
+            System.out.println(" En yakın tam sayı (round): " + Math.round(number));
+
+            sc.close();
+        }
 }
--- src/Week2Patika/SifreKontrol.java
+++ src/Week2Patika/SifreKontrol.java
@@ -1,4 +1,46 @@
 package Week2Patika;
 
-public class SifreKontrol {
+
+    import java.util.Scanner;
+
+    public class SifreKontrol {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+
+            System.out.print("Şifrenizi giriniz: ");
+            String password = scan.nextLine();
+
+            if (isValidPassword(password)) {
+                System.out.println("Geçerli Şifre");
+            } else {
+                System.out.println("Geçersiz Şifre");
+            }
+
+            scan.close();
+        }
+
+        public static boolean isValidPassword(String password) {
+            // 1. En az 8 karakter
+            if (password.length() < 8) {
+                return false;
+            }
+
+            // 2. Space karakteri içermemeli
+            if (password.contains(" ")) {
+                return false;
+            }
+
+            // 3. İlk harf büyük harf olmalı
+            char firstChar = password.charAt(0);
+            if (!(firstChar >= 'A' && firstChar <= 'Z')) {
+                return false;
+            }
+
+            // 4. Son karakter '?' olmalı
+            if (password.charAt(password.length() - 1) != '?') {
+                return false;
+            }
+
+            return true; // tüm kurallardan geçtiyse
+        }
 }
